907eb9f3e6b472fc89c5ee28dcc07366a24c8810
Waylon Brown <waylonjacebrown@gmail.com>
2026-04-22 07:40:35 -0500

    Upload via GitDash

--- utils.js
+++ utils.js
@@ -1,16 +1,47 @@
-import { derivedObservableWithCache } from '../utils/utils.js';
-
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-/**
- * Works like a derived.
- * However, if the value is not undefined, it is cached and will not be recomputed anymore.
- * In that case, the derived will unsubscribe from its dependencies.
-*/
-function derivedConstOnceDefined(owner, fn) {
-    return derivedObservableWithCache(owner, (reader, lastValue) => lastValue ?? fn(reader));
+class Throttler {
+    constructor() {
+        this._timeout = undefined;
+    }
+    throttle(fn, timeoutMs) {
+        if (this._timeout === undefined) {
+            this._timeout = setTimeout(() => {
+                this._timeout = undefined;
+                fn();
+            }, timeoutMs);
+        }
+    }
+    dispose() {
+        if (this._timeout !== undefined) {
+            clearTimeout(this._timeout);
+        }
+    }
+}
+function deepAssign(target, source) {
+    for (const key in source) {
+        if (!!target[key] && typeof target[key] === 'object' && !!source[key] && typeof source[key] === 'object') {
+            deepAssign(target[key], source[key]);
+        }
+        else {
+            target[key] = source[key];
+        }
+    }
+}
+function deepAssignDeleteNulls(target, source) {
+    for (const key in source) {
+        if (source[key] === null) {
+            delete target[key];
+        }
+        else if (!!target[key] && typeof target[key] === 'object' && !!source[key] && typeof source[key] === 'object') {
+            deepAssignDeleteNulls(target[key], source[key]);
+        }
+        else {
+            target[key] = source[key];
+        }
+    }
 }
 
-export { derivedConstOnceDefined };
+export { Throttler, deepAssign, deepAssignDeleteNulls };
