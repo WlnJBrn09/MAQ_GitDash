e66031e3941ce32c26015746565d52c5fa2e74fc
Waylon Brown <waylonjacebrown@gmail.com>
2026-04-22 07:38:22 -0500

    Upload via GitDash

--- strings.js
+++ strings.js
@@ -17,7 +17,7 @@ function startsWith(haystack, needle) {
  * Determines if haystack ends with needle.
  */
 function endsWith(haystack, needle) {
-    let diff = haystack.length - needle.length;
+    const diff = haystack.length - needle.length;
     if (diff > 0) {
         return haystack.lastIndexOf(needle) === diff;
     }
@@ -27,66 +27,6 @@ function endsWith(haystack, needle) {
     else {
         return false;
     }
-}
-/**
- * Computes the difference score for two strings. More similar strings have a higher score.
- * We use largest common subsequence dynamic programming approach but penalize in the end for length differences.
- * Strings that have a large length difference will get a bad default score 0.
- * Complexity - both time and space O(first.length * second.length)
- * Dynamic programming LCS computation http://en.wikipedia.org/wiki/Longest_common_subsequence_problem
- *
- * @param first a string
- * @param second a string
- */
-function difference(first, second, maxLenDelta = 4) {
-    let lengthDifference = Math.abs(first.length - second.length);
-    // We only compute score if length of the currentWord and length of entry.name are similar.
-    if (lengthDifference > maxLenDelta) {
-        return 0;
-    }
-    // Initialize LCS (largest common subsequence) matrix.
-    let LCS = [];
-    let zeroArray = [];
-    let i, j;
-    for (i = 0; i < second.length + 1; ++i) {
-        zeroArray.push(0);
-    }
-    for (i = 0; i < first.length + 1; ++i) {
-        LCS.push(zeroArray);
-    }
-    for (i = 1; i < first.length + 1; ++i) {
-        for (j = 1; j < second.length + 1; ++j) {
-            if (first[i - 1] === second[j - 1]) {
-                LCS[i][j] = LCS[i - 1][j - 1] + 1;
-            }
-            else {
-                LCS[i][j] = Math.max(LCS[i - 1][j], LCS[i][j - 1]);
-            }
-        }
-    }
-    return LCS[first.length][second.length] - Math.sqrt(lengthDifference);
-}
-/**
- * Limit of string length.
- */
-function getLimitedString(str, ellipsis = true) {
-    if (!str) {
-        return '';
-    }
-    if (str.length < 140) {
-        return str;
-    }
-    return str.slice(0, 140) + (ellipsis ? '\u2026' : '');
-}
-/**
- * Limit of string length.
- */
-function trim(str, regexp) {
-    const m = regexp.exec(str);
-    if (m && m[0].length) {
-        return str.substr(0, str.length - m[0].length);
-    }
-    return str;
 }
 function repeat(value, count) {
     let s = '';
@@ -99,5 +39,15 @@ function repeat(value, count) {
     }
     return s;
 }
+const _a = 'a'.charCodeAt(0);
+const _z = 'z'.charCodeAt(0);
+const _A = 'A'.charCodeAt(0);
+const _Z = 'Z'.charCodeAt(0);
+const _0 = '0'.charCodeAt(0);
+const _9 = '9'.charCodeAt(0);
+function isLetterOrDigit(text, index) {
+    const c = text.charCodeAt(index);
+    return (_a <= c && c <= _z) || (_A <= c && c <= _Z) || (_0 <= c && c <= _9);
+}
 
-export { difference, endsWith, getLimitedString, repeat, startsWith, trim };
+export { endsWith, isLetterOrDigit, repeat, startsWith };
