bd58f3c9c407d70b0b15d8141221dc7879be1dfd
Waylon Brown <waylonjacebrown@gmail.com>
2026-04-22 07:38:10 -0500

    Upload via GitDash

--- dataManager.js
+++ dataManager.js
@@ -1,90 +1,80 @@
-import { values } from '../utils/objects.js';
-import { cssData } from '../data/webCustomData.js';
-import { CSSDataProvider } from './dataProvider.js';
+import { HTMLDataProvider } from './dataProvider.js';
+import { htmlData } from './data/webCustomData.js';
+import { binarySearch } from '../utils/arrays.js';
 
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-class CSSDataManager {
+class HTMLDataManager {
     constructor(options) {
         this.dataProviders = [];
-        this._propertySet = {};
-        this._atDirectiveSet = {};
-        this._pseudoClassSet = {};
-        this._pseudoElementSet = {};
-        this._properties = [];
-        this._atDirectives = [];
-        this._pseudoClasses = [];
-        this._pseudoElements = [];
-        this.setDataProviders(options?.useDefaultDataProvider !== false, options?.customDataProviders || []);
+        this.setDataProviders(options.useDefaultDataProvider !== false, options.customDataProviders || []);
     }
     setDataProviders(builtIn, providers) {
         this.dataProviders = [];
         if (builtIn) {
-            this.dataProviders.push(new CSSDataProvider(cssData));
+            this.dataProviders.push(new HTMLDataProvider('html5', htmlData));
         }
         this.dataProviders.push(...providers);
-        this.collectData();
     }
-    /**
-     * Collect all data  & handle duplicates
-     */
-    collectData() {
-        this._propertySet = {};
-        this._atDirectiveSet = {};
-        this._pseudoClassSet = {};
-        this._pseudoElementSet = {};
-        this.dataProviders.forEach(provider => {
-            provider.provideProperties().forEach(p => {
-                if (!this._propertySet[p.name]) {
-                    this._propertySet[p.name] = p;
-                }
-            });
-            provider.provideAtDirectives().forEach(p => {
-                if (!this._atDirectiveSet[p.name]) {
-                    this._atDirectiveSet[p.name] = p;
-                }
-            });
-            provider.providePseudoClasses().forEach(p => {
-                if (!this._pseudoClassSet[p.name]) {
-                    this._pseudoClassSet[p.name] = p;
-                }
-            });
-            provider.providePseudoElements().forEach(p => {
-                if (!this._pseudoElementSet[p.name]) {
-                    this._pseudoElementSet[p.name] = p;
-                }
-            });
+    getDataProviders() {
+        return this.dataProviders;
+    }
+    isVoidElement(e, voidElements) {
+        return !!e && binarySearch(voidElements, e.toLowerCase(), (s1, s2) => s1.localeCompare(s2)) >= 0;
+    }
+    getVoidElements(languageOrProviders) {
+        const dataProviders = Array.isArray(languageOrProviders) ? languageOrProviders : this.getDataProviders().filter(p => p.isApplicable(languageOrProviders));
+        const voidTags = [];
+        dataProviders.forEach((provider) => {
+            provider.provideTags().filter(tag => tag.void).forEach(tag => voidTags.push(tag.name));
         });
-        this._properties = values(this._propertySet);
-        this._atDirectives = values(this._atDirectiveSet);
-        this._pseudoClasses = values(this._pseudoClassSet);
-        this._pseudoElements = values(this._pseudoElementSet);
+        return voidTags.sort();
     }
-    getProperty(name) { return this._propertySet[name]; }
-    getAtDirective(name) { return this._atDirectiveSet[name]; }
-    getPseudoClass(name) { return this._pseudoClassSet[name]; }
-    getPseudoElement(name) { return this._pseudoElementSet[name]; }
-    getProperties() {
-        return this._properties;
-    }
-    getAtDirectives() {
-        return this._atDirectives;
-    }
-    getPseudoClasses() {
-        return this._pseudoClasses;
-    }
-    getPseudoElements() {
-        return this._pseudoElements;
-    }
-    isKnownProperty(name) {
-        return name.toLowerCase() in this._propertySet;
-    }
-    isStandardProperty(name) {
-        return this.isKnownProperty(name) &&
-            (!this._propertySet[name.toLowerCase()].status || this._propertySet[name.toLowerCase()].status === 'standard');
+    isPathAttribute(tag, attr) {
+        // should eventually come from custom data
+        if (attr === 'src' || attr === 'href') {
+            return true;
+        }
+        const a = PATH_TAG_AND_ATTR[tag];
+        if (a) {
+            if (typeof a === 'string') {
+                return a === attr;
+            }
+            else {
+                return a.indexOf(attr) !== -1;
+            }
+        }
+        return false;
     }
 }
+// Selected from https://stackoverflow.com/a/2725168/1780148
+const PATH_TAG_AND_ATTR = {
+    // HTML 4
+    a: 'href',
+    area: 'href',
+    body: 'background',
+    blockquote: 'cite',
+    del: 'cite',
+    form: 'action',
+    frame: ['src', 'longdesc'],
+    img: ['src', 'longdesc'],
+    ins: 'cite',
+    link: 'href',
+    object: 'data',
+    q: 'cite',
+    script: 'src',
+    // HTML 5
+    audio: 'src',
+    button: 'formaction',
+    command: 'icon',
+    embed: 'src',
+    html: 'manifest',
+    input: ['src', 'formaction'],
+    source: 'src',
+    track: 'src',
+    video: ['src', 'poster']
+};
 
-export { CSSDataManager };
+export { HTMLDataManager };
