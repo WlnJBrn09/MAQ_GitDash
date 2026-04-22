72925c0775180f13f482fce4545e2b95bd2d018c
Waylon Brown <waylonjacebrown@gmail.com>
2026-04-22 07:38:11 -0500

    Upload via GitDash

--- dataProvider.js
+++ dataProvider.js
@@ -1,74 +1,115 @@
+import { normalizeMarkupContent } from '../utils/markup.js';
+
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-class CSSDataProvider {
+class HTMLDataProvider {
+    isApplicable() {
+        return true;
+    }
     /**
      * Currently, unversioned data uses the V1 implementation
      * In the future when the provider handles multiple versions of HTML custom data,
      * use the latest implementation for unversioned data
      */
-    constructor(data) {
-        this._properties = [];
-        this._atDirectives = [];
-        this._pseudoClasses = [];
-        this._pseudoElements = [];
-        this.addData(data);
-    }
-    provideProperties() {
-        return this._properties;
-    }
-    provideAtDirectives() {
-        return this._atDirectives;
-    }
-    providePseudoClasses() {
-        return this._pseudoClasses;
-    }
-    providePseudoElements() {
-        return this._pseudoElements;
-    }
-    addData(data) {
-        if (Array.isArray(data.properties)) {
-            for (const prop of data.properties) {
-                if (isPropertyData(prop)) {
-                    this._properties.push(prop);
-                }
-            }
-        }
-        if (Array.isArray(data.atDirectives)) {
-            for (const prop of data.atDirectives) {
-                if (isAtDirective(prop)) {
-                    this._atDirectives.push(prop);
-                }
-            }
-        }
-        if (Array.isArray(data.pseudoClasses)) {
-            for (const prop of data.pseudoClasses) {
-                if (isPseudoClassData(prop)) {
-                    this._pseudoClasses.push(prop);
-                }
-            }
-        }
-        if (Array.isArray(data.pseudoElements)) {
-            for (const prop of data.pseudoElements) {
-                if (isPseudoElementData(prop)) {
-                    this._pseudoElements.push(prop);
-                }
-            }
+    constructor(id, customData) {
+        this.id = id;
+        this._tags = [];
+        this._tagMap = {};
+        this._valueSetMap = {};
+        this._tags = customData.tags || [];
+        this._globalAttributes = customData.globalAttributes || [];
+        this._tags.forEach(t => {
+            this._tagMap[t.name.toLowerCase()] = t;
+        });
+        if (customData.valueSets) {
+            customData.valueSets.forEach(vs => {
+                this._valueSetMap[vs.name] = vs.values;
+            });
         }
     }
+    getId() {
+        return this.id;
+    }
+    provideTags() {
+        return this._tags;
+    }
+    provideAttributes(tag) {
+        const attributes = [];
+        const processAttribute = (a) => {
+            attributes.push(a);
+        };
+        const tagEntry = this._tagMap[tag.toLowerCase()];
+        if (tagEntry) {
+            tagEntry.attributes.forEach(processAttribute);
+        }
+        this._globalAttributes.forEach(processAttribute);
+        return attributes;
+    }
+    provideValues(tag, attribute) {
+        const values = [];
+        attribute = attribute.toLowerCase();
+        const processAttributes = (attributes) => {
+            attributes.forEach(a => {
+                if (a.name.toLowerCase() === attribute) {
+                    if (a.values) {
+                        a.values.forEach(v => {
+                            values.push(v);
+                        });
+                    }
+                    if (a.valueSet) {
+                        if (this._valueSetMap[a.valueSet]) {
+                            this._valueSetMap[a.valueSet].forEach(v => {
+                                values.push(v);
+                            });
+                        }
+                    }
+                }
+            });
+        };
+        const tagEntry = this._tagMap[tag.toLowerCase()];
+        if (tagEntry) {
+            processAttributes(tagEntry.attributes);
+        }
+        processAttributes(this._globalAttributes);
+        return values;
+    }
 }
-function isPropertyData(d) {
-    return typeof d.name === 'string';
-}
-function isAtDirective(d) {
-    return typeof d.name === 'string';
-}
-function isPseudoClassData(d) {
-    return typeof d.name === 'string';
-}
-function isPseudoElementData(d) {
-    return typeof d.name === 'string';
+/**
+ * Generate Documentation used in hover/complete
+ * From `documentation` and `references`
+ */
+function generateDocumentation(item, settings = {}, doesSupportMarkdown) {
+    const result = {
+        kind: doesSupportMarkdown ? 'markdown' : 'plaintext',
+        value: ''
+    };
+    if (item.description && settings.documentation !== false) {
+        const normalizedDescription = normalizeMarkupContent(item.description);
+        if (normalizedDescription) {
+            result.value += normalizedDescription.value;
+        }
+    }
+    if (item.references && item.references.length > 0 && settings.references !== false) {
+        if (result.value.length) {
+            result.value += `\n\n`;
+        }
+        if (doesSupportMarkdown) {
+            result.value += item.references.map(r => {
+                return `[${r.name}](${r.url})`;
+            }).join(' | ');
+        }
+        else {
+            result.value += item.references.map(r => {
+                return `${r.name}: ${r.url}`;
+            }).join('\n');
+        }
+    }
+    if (result.value === '') {
+        return undefined;
+    }
+    return result;
 }
 
-export { CSSDataProvider };
+export { HTMLDataProvider, generateDocumentation };
